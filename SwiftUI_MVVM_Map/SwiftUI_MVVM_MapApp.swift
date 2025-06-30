042177224f1383e4249fe2bc99e0b24c844fe24b
Arpit Kumar <saini.arpit38@gmail.com>
2025-06-30 10:50:56 +0530

    Initial Commit

---
+++ SwiftUI_MVVM_Map/SwiftUI_MVVM_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVM_MapApp.swift
+//  SwiftUI_MVVM_Map
+//
+//  Created by Entab on 30/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVM_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
